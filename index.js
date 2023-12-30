053aaef5b0693640c2dd1c90623aef341b2b9c63
JanusZhu <janus.zhu@aya.yale.edu>
2023-12-30 15:43:54 -0800

    finished 3.2

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  const numberOfPeople = persons.length;
+  response.send(
+    `<p>Phonebook has info for ${numberOfPeople} people</p> <p>${new Date()}</p>`
+  );
+});
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
